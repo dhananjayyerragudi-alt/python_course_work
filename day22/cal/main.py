19979b5bdcd0398b39e05bc2abfd555437dd0773
Dhananjaya Reddy <dhananjay.yerragudi@gmail.com>
2026-05-22 19:24:50 +0530

    Add files via upload

---
+++ day22/cal/main.py
@@ -0,0 +1,18 @@
+'''
+import logic as x
+
+print(x.add(5,6))
+print(x.sub(5,6))
+print(x.mul(5,6))
+print(x.exp(5,6))
+'''
+'''
+from logic import add,div
+print(add(5,6))
+print(div(5,6))
+'''
+'''
+from logic import *
+print(add(5,6))
+print(sub(5,6))
+'''
